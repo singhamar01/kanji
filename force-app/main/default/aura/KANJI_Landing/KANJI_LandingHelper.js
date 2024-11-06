68c5c39377d6c83b546ac25a06416064f5f21bc5
nova-miniPC <singhamar01@gmail.com>
2024-11-06 21:36:08 +0900

    gsheet integration initial

--- force-app/main/default/aura/KANJI_Landing/KANJI_LandingHelper.js
+++ force-app/main/default/aura/KANJI_Landing/KANJI_LandingHelper.js
@@ -30,8 +30,8 @@
 				data.forEach(element => {
 					element.examples.forEach(example_row =>{
 						if(example_row.sentence != undefined){
-							example_row.sentence = example_row.sentence.replace(example_row.example.trim(),'<mark class="highlighter">'+example_row.example.trim()+'</mark>');
-							example_row.sentence_reading = example_row.sentence_reading.replace(example_row.example_reading.trim(),'<mark class="highlighter">'+example_row.example_reading.trim()+'</mark>');
+							example_row.sentence = example_row.sentence.replaceAll(example_row.example.trim(),'<span class="highlighter">'+example_row.example.trim()+'<sup>'+example_row.example_reading.trim()+'</sup></span>');
+							example_row.sentence_reading = example_row.sentence_reading.replaceAll(example_row.example_reading.trim(),'<span class="reading_highlighter">'+example_row.example_reading.trim()+'</span>');
 						}
 					});
 				});
@@ -44,6 +44,83 @@
         })).catch($A.getCallback((error) => {
             console.error('Fetch Error :-S', error);
         }));
+	},
+	/***************************************************
+   	** Name : fetchPharseListFromGSheet
+   	** Purpose : to show toast
+   	** Created by :SAMYA 
+   	** Updated by :   	
+   	*****************************************************/	
+	fetchPharseListFromGSheet : function(component, event, helper) {
+		let localContext = this;
+		const gsheet_id = $A.get("$Label.c.GSHEET_ID"); 
+		const gsheet_key = $A.get("$Label.c.GSHEET_KEY"); 
+		const sheetEndpoint = "https://www.googleapis.com/drive/v3/files/"+gsheet_id+"/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet&key="+gsheet_key;    
+		fetch(sheetEndpoint).then(function(response){
+			return response.arrayBuffer();
+		}).then($A.getCallback((buffer) => {
+			let workbook = XLSX.read(buffer);						
+			let data = localContext.prepareJSON(workbook);
+			//console.log(JSON.stringify(data));
+			//---------------------------------------//
+			data.forEach(element => {				
+				element.examples.forEach(example_row =>{
+					if(example_row.sentence != undefined){
+						example_row.sentence = example_row.sentence.replaceAll(example_row.example.trim(),'<span class="highlighter">'+example_row.example.trim()+'<sup>'+example_row.example_reading.trim()+'</sup></span>');
+						example_row.sentence_reading = example_row.sentence_reading.replaceAll(example_row.example_reading.trim(),'<span class="reading_highlighter">'+example_row.example_reading.trim()+'</span>');
+					}
+				});
+			});
+			
+			//---------------------------------------//
+			localContext.calculate(component, data);
+			
+		}));		
+	},
+     /***************************************************
+   	** Name : calculate
+   	** Purpose : to show toast
+   	** Created by :SAMYA 
+   	** Updated by :   	
+   	*****************************************************/	
+    calculate : function(component, data) {
+            component.set('v.pharseList', data);
+        console.log('iteration done in calculate');
+			this.KANJI_DATA = data;
+			console.log('iteration done');
+			component.set('v.displayPharseList', data);		
+			component.set('v.display_count', data.length+'');
+    },
+	/***************************************************
+   	** Name : prepareJSON
+   	** Purpose : to show toast
+   	** Created by :SAMYA 
+   	** Updated by :   	
+   	*****************************************************/	
+	prepareJSON : function(workbook) {
+		let detail_data = XLSX.utils.sheet_to_json(workbook.Sheets['detail']);        
+        //-----------------------------------------------//
+        //fetching [example] sheet
+        let example_data = XLSX.utils.sheet_to_json(workbook.Sheets['example']);      
+		//-----------------------------------------------//
+        //creating map using [key]
+        let detail_data_MAP = Object.groupBy(detail_data, function( item){return item.key;}); 
+        let example_data_MAP = Object.groupBy(example_data, function( item){return item.key;});
+		//------------------------------------------------//
+		let jsonDataList = [];        
+        //Object.keys(detail_data_MAP).forEach( async (key, logIndex) =>{              
+        for(let key of Object.keys(detail_data_MAP)) {          
+            let element = detail_data_MAP[key][0];//since group creates array            
+			delete element['row_num'];
+			delete element['stroke_parse'];
+			element['examples'] = (example_data_MAP[key] != undefined)?example_data_MAP[key]:[];
+			element.kunyomi = (element.kunyomi != undefined)?element.kunyomi.split('\r\n'):[];
+			element.onyomi = (element.onyomi != undefined)?element.onyomi.split('\r\n'):[];
+			element.example_count = element.examples.length;
+			jsonDataList.push(element);            
+        }
+        //------------------------------------------------//
+        return jsonDataList; 
 	},
 	/***************************************************
    	** Name : slide
@@ -81,7 +158,6 @@
 		let element_row_indx = parseInt(component.get('v.carousel_index'),10);		
 		let mode = event.currentTarget.name;		
 		let focusedElement = component.get('v.displayPharseList')[element_row_indx];
-        console.log(mode);
 		component.set('v.mode', mode);
 		component.set('v.focusedElement', focusedElement);
 		component.set("v.showPopup", true);
@@ -162,29 +238,43 @@
    	** Updated by :   	
    	*****************************************************/	
 	handleSearch : function(component, event, helper) {
-		let searchValue = event.currentTarget.value;		
+		let searchValue = event.currentTarget.value;	
+		//console.log('search calling');	
 		//let kanji_data = component.get('v.pharseList');
 		let kanji_data = this.KANJI_DATA;
 		let searched_result = kanji_data.filter(function(item){
 			let return_param = false;
 			if(
-				item.key.includes(searchValue) 
-				|| item.kunyomi.toString().includes(searchValue)
-				|| item.onyomi.toString().includes(searchValue)
-				|| item.complexity.includes(searchValue)
-				|| (item.help_text != undefined && item.help_text.includes(searchValue))
-				|| (item.level == searchValue)
-				|| JSON.stringify(item.examples).includes(searchValue)
+				(item.key != undefined && item.key.includes(searchValue) )
+				|| (item.kunyomi != undefined && item.kunyomi.toString().toLowerCase().includes(searchValue.toLowerCase()))
+				|| (item.onyomi != undefined && item.onyomi.toString().toLowerCase().includes(searchValue.toLowerCase()))
+				|| (item.complexity != undefined && item.complexity.toLowerCase().includes(searchValue.toLowerCase()))
+				|| (item.help_text != undefined && item.help_text.toLowerCase().includes(searchValue.toLowerCase()))
+				|| (item.level != undefined && item.level == searchValue)
+				|| (item.examples != undefined && JSON.stringify(item.examples).toLowerCase().includes(searchValue.toLowerCase()))
 			){
 				//--------------------------------//
 				return_param = true;
 			}
 			return return_param;
 		});
-		console.log(';;'+JSON.stringify(searched_result));
-		let resultSet = (searched_result != undefined)?searched_result:[];		
-		component.set('v.displayPharseList', resultSet);		
-		component.set('v.display_count', resultSet.length+'');		
+		//console.log(';;'+JSON.stringify(searched_result));
+		let resultSet = (searched_result != undefined)?searched_result:[];	
+		//---------------------------------------------------//
+		let highlightedResultSet = [];
+		if(searchValue != undefined && searchValue.length>0){
+			resultSet.forEach((item) =>{
+				let itemString = JSON.stringify(item);
+				itemString = itemString.replaceAll(searchValue,'<mark>'+searchValue+'</mark>');				
+				highlightedResultSet.push(JSON.parse(itemString));
+			});
+		}
+		else{
+			highlightedResultSet = resultSet;
+		}
+		//---------------------------------------------------//
+		component.set('v.displayPharseList', highlightedResultSet);		
+		component.set('v.display_count', highlightedResultSet.length+'');		
 		this.resetSearch(component, event, helper);
 	},
 	/***************************************************
